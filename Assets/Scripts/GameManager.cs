b87149b7b06772d0700ee233b1951a22c327b4f3
notbaopham <113058628+notbaopham@users.noreply.github.com>
2025-02-15 21:36:51 -0800

    Added Scoring System

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private FallTrigger[] pins;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pins = FindObjectsByType<FallTrigger>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+        foreach (FallTrigger pin in pins)
+        {
+            pin.OnPinFall.AddListener(IncrementScore);
+        }
+    }
+
+    private void IncrementScore()
+    {
+        score++;
+        scoreText.text = $"Score: {score}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
