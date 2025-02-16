e5ea6a5d4d292d183a07be82fc271ee6c6bd5c5d
notbaopham <113058628+notbaopham@users.noreply.github.com>
2025-02-15 17:34:52 -0800

    Gutter, Pins and Ball created
    Along with functional scripts for the Game.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    // Method for declaring a new UnityEvent when the Space is pressed
+    public UnityEvent OnSpacePressed = new UnityEvent();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            {
+                OnSpacePressed?.Invoke();
+            }
+    }
+}
